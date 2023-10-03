3d9ab93c16db235fb8bc538803fdb7d9be66561b
florian <florian.batt@hotmail.fr>
2023-10-03 21:40:55 +0200

    fb - added logo imgs and displayProductList

--- script.js
+++ script.js
@@ -1,9 +1,14 @@
 let content = document.getElementById("content");
 let content2 = document.getElementById("content2");
+let getSidebarContent = document.getElementById("sidebar-content");
+let productorsData = [];
+let productsData = [];
+let displayProductorsListDone = false;
 
 fetch("https://loci.directus.app/items/productor")
   .then((response) => response.json())
   .then((data) => {
+    productorsData = data["data"];
     for (let i = 0; i < data["data"].length; i++) {
       const p = document.createElement("p");
       p.innerHTML = data["data"][i].name;
@@ -15,12 +20,13 @@ fetch("https://loci.directus.app/items/productor")
     }
   });
 
-fetch("https://loci.directus.app/items/product")
+fetch("https://loci.directus.app/items/Product")
   .then((response) => response.json())
   .then((data) => {
+    productsData = data["data"];
     for (let i = 0; i < data["data"].length; i++) {
       const p = document.createElement("p");
-      p.innerHTML = data["data"][i].name;
+      p.innerHTML = data["data"][i].Name;
       content2.insertAdjacentHTML("afterbegin", p.outerHTML);
     }
   });
@@ -29,6 +35,49 @@ function background(newImageUrl) {
   var mainSection = document.getElementById("main");
   mainSection.style.backgroundImage = 'url("' + newImageUrl + '")';
 }
+
+function displayProductorsList() {
+  if (displayProductorsListDone == false) {
+    // Faire tomber l'opacité du texte à 0 en 0.5 seconde
+    getSidebarContent.style.transition = "opacity 0.5s";
+    getSidebarContent.style.opacity = "0";
+
+    // Utiliser setTimeout pour attendre que la transition soit terminée avant de supprimer le contenu
+    setTimeout(() => {
+      getSidebarContent.innerHTML = "";
+
+      // Afficher le nouveau contenu à une opacité de 0
+      for (let i = 0; i < productsData.length; i++) {
+        const p = document.createElement("p");
+        p.style.marginBottom = "0px";
+        p.style.paddingTop = "16px";
+        p.style.paddingBottom = "16px";
+        p.innerHTML = productsData[i].Productor;
+        getSidebarContent.insertAdjacentHTML("afterbegin", p.outerHTML);
+
+        // Ajouter un séparateur gris après chaque producteur
+        if (i < productsData.length - 1) {
+          // Ne pas ajouter de séparateur après le dernier producteur
+          const hr = document.createElement("hr");
+          hr.style.borderTop = "1px solid gray"; // Définir la couleur du séparateur en gris
+          hr.style.marginBottom = "0px";
+          getSidebarContent.insertAdjacentHTML("afterbegin", hr.outerHTML);
+        }
+      }
+
+      // Augmenter l'opacité à 1 en 0.5 seconde
+      getSidebarContent.style.opacity = "1";
+    }, 500); // Attendre 500 ms (0.5 seconde) avant d'exécuter cette fonction
+    displayProductorsListDone = true;
+  }
+}
+
+var tooltipTriggerList = [].slice.call(
+  document.querySelectorAll('[data-bs-toggle="tooltip"]')
+);
+var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
+  return new bootstrap.Tooltip(tooltipTriggerEl);
+});
 
 // Effect slide to left
 

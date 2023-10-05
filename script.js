941b84cb47a6b3b8833ea21eb3aee9ea90edfda4
florian <florian.batt@hotmail.fr>
2023-10-05 12:20:26 +0200

    fb - style changes

--- script.js
+++ script.js
@@ -160,12 +160,12 @@ function displayProductorsList() {
             popupContent = `
             <button id="close-popup" style="position: absolute; right: 10px; top: 10px;">X</button>
             <span>${notePopup}</span>
-            <h2>${productorsData[i].name}</h2>
+            <h3>${productorsData[i].name}</h3>
             <p>${stall.map((x) => x.outerHTML).join("")}</p>
             <hr style="border:1px solid grey">
             <img src='images/feuilles-loci.png' alt='description' style='height:16px' /><img src='images/feuilles-loci.png' alt='description' style='height:16px' />
             <span style="color:green; float:right">Conseillé</span>
-            <h2>${advisedProductor[0].name}</h2>
+            <h3>${advisedProductor[0].name}</h3>
             <p>${stallAdvisedProductor.map((x) => x.outerHTML).join("")}</p>
             
 
@@ -174,11 +174,11 @@ function displayProductorsList() {
             popupContent = `
             <button id="close-popup" style="position: absolute; right: 10px; top: 10px;">X</button>
             <span>${notePopup}</span>
-            <h2>${productorsData[i].name}</h2>
+            <h3>${productorsData[i].name}</h3>
             <p>${stall.map((x) => x.outerHTML).join("")}</p>
             <hr style="border:1px solid grey">
             <img src='images/feuilles-loci.png' alt='description' style='height:16px' />
-            <h2>${supermarket[0].name}</h2>
+            <h3>${supermarket[0].name}</h3>
             <p>${stallSupermarket.map((x) => x.outerHTML).join("")}</p>
           `;
           }
